95f65733ebcd01ecb4ab44f9ea4ded5a0e77403a
LeynardPenaranda <leynardlove@gmail.com>
2025-08-14 09:43:12 +0800

    add post api route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,10 +1,45 @@
 import express from "express";
-
+import auth from "../../middleware/auth.js";
+import { check, validationResult } from "express-validator";
+import User from "../../models/User.js";
+import Profile from "../../models/Profile.js";
+import Post from "../../models/Post.js";
 const router = express.Router();
 
-// @route GET api/posts
-// @desc Test route
-// @access Public
-router.get("/", (req, res) => res.send("posts route"));
+// @route POST api/posts
+// @desc Create a post
+// @access Private
+router.post(
+  "/",
+  [auth, [check("text", "Text is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      // get the user from the token
+      const user = await User.findById(req.user.id).select("-password");
+
+      // create an object for post model
+      const newPost = new Post({
+        user: req.user.id,
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+      });
+
+      // save the post into the database
+      const post = await newPost.save();
+      //return the post by response
+      res.json(post);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).json({ msg: "Server Error" });
+    }
+  }
+);
 
 export default router;
