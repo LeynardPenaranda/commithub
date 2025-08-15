c90c51216322e0c5db965e2a4cd14cbe81bfe7eb
LeynardPenaranda <leynardlove@gmail.com>
2025-08-15 15:10:40 +0800

    setup redux store

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,8 @@ import AppLayout from "./pages/layout/App-layout";
 import Developer from "./pages/developer-page";
 import Login from "./pages/login-page";
 import Register from "./pages/register-page";
+import { Provider } from "react-redux";
+import store from "./store";
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
@@ -29,7 +31,11 @@ const router = createBrowserRouter([
 ]);
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  );
 };
 
 export default App;
